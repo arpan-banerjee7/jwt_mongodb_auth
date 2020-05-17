637d3a1322c75617adc3689796238973690c44e9
Arpan619Banerjee <arpan.nbanerjee7@gmail.com>
2020-05-17 13:50:10 +0530

    3.Added the landing page and set up the required routes

--- ngApp/src/app/app-routing.module.ts
+++ ngApp/src/app/app-routing.module.ts
@@ -1,11 +1,21 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { EventsComponent } from "./events/events.component";
+import { LoginComponent } from "./login/login.component";
+import { RegisterComponent } from "./register/register.component";
+import { SpecialEventsComponent } from "./special-events/special-events.component";
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", redirectTo: "events", pathMatch: "full" },
+  { path: "events", component: EventsComponent },
+  { path: "login", component: LoginComponent },
+  { path: "register", component: RegisterComponent },
+  { path: "special", component: SpecialEventsComponent },
+  { path: "**", redirectTo: "/login" },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
